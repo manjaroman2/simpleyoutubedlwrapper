d8217f92a5bb06af36c988c5a67aebe65ab2d59b
manjaroman <manjaroman@protonmail.com>
2023-02-03 15:46:39 +0100

    add config ifle

--- src/sampdl.py
+++ src/sampdl.py
@@ -8,41 +8,30 @@ from flask import (
     make_response,
 )
 from yt_dlp import YoutubeDL, DownloadError
-from yt_dlp.utils import parse_codecs
 import re
-from pathlib import Path
 from uuid import uuid4
 import random
 from time import time, ctime
 import shutil
 from threading import Thread
 from queue import Queue
-import datetime
 
+from config import * 
 
 random.seed(time())
-
-SECRET_KEY = "3e4614e803a6bfa5112705b7"
-
-
 urlregex = re.compile(
     r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
 )
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
-datadir = Path().cwd() / "data"
-if datadir.is_dir():
-    shutil.rmtree(datadir)
-datadir.mkdir()
-print("> datadir:", datadir)
+if DATADIR.is_dir():
+    shutil.rmtree(DATADIR)
+DATADIR.mkdir()
+print("> DATADIR:", DATADIR)
 FILES = dict()
 LOADING = dict()
 AGE = dict()
 WORKERS = {}
-AUDIOCODECS = ["flac", "wav", "mp3"]
-VIDEOCODECS = ["mp4"]
-
-MAX_SESSION_AGE = datetime.timedelta(days=30)
 
 
 @app.route("/sampdl/dl/<string:file>")
@@ -54,7 +43,7 @@ def download(file):
             for z in FILES[sesh]:
                 if z["file"] == file:
                     return send_from_directory(
-                        datadir, z["file"], as_attachment=True, download_name=z["name"]
+                        DATADIR, z["file"], as_attachment=True, download_name=z["name"]
                     )
     return redirect(url_for("sampdl"))
 
@@ -87,7 +76,7 @@ def worker(sesh, urls, ydlopts, tmpdir, aszip):
         if len(tmpfiles) < 10:
             for tmpfile in tmpfiles:
                 fileuuid = str(uuid4())
-                shutil.copyfile(tmpfile, datadir / fileuuid)
+                shutil.copyfile(tmpfile, DATADIR / fileuuid)
                 files.append(
                     {
                         "file": fileuuid,
@@ -97,7 +86,7 @@ def worker(sesh, urls, ydlopts, tmpdir, aszip):
                 )
             skip = True
     if not skip:
-        shutil.make_archive(datadir / sesh, "zip", tmpdir)
+        shutil.make_archive(DATADIR / sesh, "zip", tmpdir)
         files.append(
             {
                 "file": sesh + ".zip",
@@ -124,7 +113,7 @@ def wrapper(posturl, codecs, extractaudio=False):
             urls = urlregex.findall(request.form["link"])
             if urls:
                 uuid = str(uuid4())
-                tmpdir = datadir / uuid
+                tmpdir = DATADIR / uuid
                 tmpdir.mkdir()
                 ydlopts = {
                     "concurrent-fragments": 4,
@@ -165,7 +154,7 @@ def wrapper(posturl, codecs, extractaudio=False):
                 del AGE[sesh]
             if sesh in FILES:
                 for f in FILES[sesh]:
-                    (datadir / f["file"]).unlink()
+                    (DATADIR / f["file"]).unlink()
                 del FILES[sesh]
 
         files_sorted = []
@@ -173,7 +162,6 @@ def wrapper(posturl, codecs, extractaudio=False):
         response = None
         if "sesh" in request.cookies:
             sesh = request.cookies["sesh"]
-            print(request.cookies)
             if sesh in AGE:
                 sesh_creation_date = AGE[sesh]
                 if (datetime.datetime.now() - sesh_creation_date) < MAX_SESSION_AGE:
@@ -225,9 +213,6 @@ def {funcname}():
     exec(pycode)
     app.add_url_rule(f"/s/{ep}", view_func=locals()[funcname], methods=["GET", "POST"])
 
-print(app.url_map)
-
-
 @app.route("/")
 @app.route("/s", methods=["GET"])
 def s():
@@ -236,7 +221,4 @@ def s():
         endpoints=[url_for(obj["funcname"]) for ep, obj in ENDPOINTS.items()],
     )
 
-
-# yt-dlp -x --audio-format wav --audio-quality 0 -o "~/music/sampledl/%(title)s.%(ext)s" --restrict-filenames $1"
-# print(help(postprocessor))
 app.run(host="0.0.0.0", debug=False)
