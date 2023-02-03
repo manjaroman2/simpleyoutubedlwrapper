c18cb91046705bddf858981c66475eb5bcca8147
manjaroman <manjaroman@protonmail.com>
2023-02-03 15:46:58 +0100

    add config iflegit add .!

---
+++ src/config.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import datetime
+
+SECRET_KEY = "3e4614e803a6bfa5112705b7"
+AUDIOCODECS = ["flac", "wav", "mp3"]
+VIDEOCODECS = ["mp4"]
+MAX_SESSION_AGE = datetime.timedelta(days=30)
+DATADIR = Path().cwd() / "data"
